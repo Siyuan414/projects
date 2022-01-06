6005bd4b4523397dc3ff3370a759f27f7f8f91cd
sy <siyuan.zhang97@gmail.com>
2022-01-06 10:59:40 -0800

    upadte

---
+++ birthday_reminder/myapp/src/data.js
@@ -0,0 +1,16 @@
+export default[
+    {
+        id: 1,
+        name: 'steven',
+        age: 29,
+        image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.independent.co.uk%2Ftopic%2Fpeople-magazine&psig=AOvVaw0uBSt_jL_EgWWHEV7fzZiQ&ust=1640287225038000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMjZl56Q-PQCFQAAAAAdAAAAABAD'
+
+    },
+
+    {
+        id:2,
+        name: 'james',
+        age:23,
+        image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.healthline.com%2Fhealth%2Fmental-health%2Fwhy-people-cope-with-life-events-so-differently&psig=AOvVaw0uBSt_jL_EgWWHEV7fzZiQ&ust=1640287225038000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMjZl56Q-PQCFQAAAAAdAAAAABAJ'
+    }
+]
